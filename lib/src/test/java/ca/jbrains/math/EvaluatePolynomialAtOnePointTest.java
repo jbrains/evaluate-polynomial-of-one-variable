861e05b16a9e68cdff85c0ce4ee0ea6b40b20c52
J. B. Rainsberger <me@jbrains.ca>
2022-02-04 16:33:43 -0400

    Removed duplication by replacing hardcoded literals with the corresponding expressions.

--- lib/src/test/java/ca/jbrains/math/EvaluatePolynomialAtOnePointTest.java
+++ lib/src/test/java/ca/jbrains/math/EvaluatePolynomialAtOnePointTest.java
@@ -33,7 +33,7 @@ public class EvaluatePolynomialAtOnePointTest {
         }
 
         public int at(int point) {
-            return coefficients.length == 2 ? 5 * 2 + 0 : coefficients[0];
+            return coefficients.length == 2 ? coefficients[1] * point + coefficients[0] : coefficients[0];
         }
     }
 }
