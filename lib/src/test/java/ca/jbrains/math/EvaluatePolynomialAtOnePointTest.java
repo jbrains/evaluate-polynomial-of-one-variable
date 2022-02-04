4c3905f21925b31fa60ebd4542d5b294e792cbcc
J. B. Rainsberger <me@jbrains.ca>
2022-02-04 16:24:13 -0400

    All constant functions are constant.

--- lib/src/test/java/ca/jbrains/math/EvaluatePolynomialAtOnePointTest.java
+++ lib/src/test/java/ca/jbrains/math/EvaluatePolynomialAtOnePointTest.java
@@ -1,5 +1,7 @@
 package ca.jbrains.math;
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +11,12 @@ public class EvaluatePolynomialAtOnePointTest {
         Assertions.assertEquals(5, polynomialOfAt(5, 2));
     }
 
+    @Property
+    void constantIsConstant(@ForAll int zerothPowerCoefficient, @ForAll int atPoint) {
+        Assertions.assertEquals(zerothPowerCoefficient, polynomialOfAt(zerothPowerCoefficient, atPoint));
+    }
+
     private int polynomialOfAt(int zerothPowerCoefficient, int point) {
-        return 5;
+        return zerothPowerCoefficient;
     }
 }
