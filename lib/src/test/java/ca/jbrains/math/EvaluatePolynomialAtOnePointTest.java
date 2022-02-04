af183524cd459c88698e2334969967dc44632423
J. B. Rainsberger <me@jbrains.ca>
2022-02-04 17:03:54 -0400

    Began replacing ?: with intention-revealing if statement.

--- lib/src/test/java/ca/jbrains/math/EvaluatePolynomialAtOnePointTest.java
+++ lib/src/test/java/ca/jbrains/math/EvaluatePolynomialAtOnePointTest.java
@@ -59,6 +59,7 @@ public class EvaluatePolynomialAtOnePointTest {
         }
 
         public int at(int point) {
+            if (coefficients.length() == 0) return 0;
             return coefficients.length() == 0 ? 0
                     : coefficients.length() == 1 ? coefficients.last() * foo(point, 0) + Polynomial.of(coefficients.init()).at(point)
                     : coefficients.length() == 2 ? coefficients.last() * foo(point, 1) + Polynomial.of(coefficients.init()).at(point)
